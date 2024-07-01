7156b7ba1a3c08b9814269d540a1a06fb75b680e
Kevinzhu0 <zhjj0117@163.com>
2024-07-01 17:03:46 +0100

    github commit -m 'updated application sourcecode'

--- test/updated_app_test.py
+++ test/updated_app_test.py
@@ -185,30 +185,6 @@ def update_task_content(selected_task):
                 style={'margin-bottom': '30px'}
             ),
         ])
-    elif selected_task == 'tabular':
-        return html.Div([
-            html.Label('Select Visualization:', style={'margin-bottom': '15px'}),
-            dcc.Dropdown(
-                id='visualization-dropdown',
-                # options=visualization_options,
-                value=[],  # 默认值为空
-                multi=True,
-                className='mt-3',
-                style={'margin-bottom': '30px'}
-            ),
-            html.Label('Number of figures per row:', style={'margin-bottom': '15px'}),
-            dcc.Dropdown(
-                id='figures-per-row-dropdown',
-                options=[
-                    {'label': '1', 'value': 1},
-                    {'label': '2', 'value': 2}
-                ],
-                value=1,
-                multi=False,
-                className='mt-3',
-                style={'margin-bottom': '30px'}
-            ),
-        ])
     elif selected_task == 'vision':
         return html.Div([
             html.Label('Select Visualization:', style={'margin-bottom': '15px'}),
